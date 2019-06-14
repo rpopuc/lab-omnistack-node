19178414128a27dc340fbcacf066f8375bfed7c4
Robson P Oliveira <rpopuc@gmail.com>
2019-06-14 05:37:17 -0300

    Adiciona estilo ao Feed

--- frontend/src/pages/Feed.js
+++ frontend/src/pages/Feed.js
@@ -1,9 +1,72 @@
 import React, { Component } from 'react';
 
+import './Feed.css';
+
+import more from '../assets/more.svg';
+import like from '../assets/like.svg';
+import comment from '../assets/comment.svg';
+import send from '../assets/send.svg';
+
+
 class Feed extends Component {
     render() {
         return (
-            <h1>Feed</h1>
+            <section id="post-list">
+                <article>
+                    <header>
+                        <div className="user-info">
+                            <span>Diego Fernandes</span>
+                            <span className="place">Rio do Sul</span>
+                        </div>
+
+                        <img src={more} alt="Mais" />
+                    </header>
+
+                    <img src="http://localhost:3333/files/captura.jpg" alt=""/>
+
+                    <footer>
+                        <div className="actions">
+                            <img src={like} alt=""/>
+                            <img src={comment} alt=""/>
+                            <img src={send} alt=""/>
+                        </div>
+
+                        <strong>900 curtidas</strong>
+
+                        <p>
+                            Um post muito massa.
+                            <span>#react #omnistack #top</span>
+                        </p>
+                    </footer>
+                </article>
+                <article>
+                    <header>
+                        <div className="user-info">
+                            <span>Diego Fernandes</span>
+                            <span className="place">Rio do Sul</span>
+                        </div>
+
+                        <img src={more} alt="Mais" />
+                    </header>
+
+                    <img src="http://localhost:3333/files/captura.jpg" alt=""/>
+
+                    <footer>
+                        <div className="actions">
+                            <img src={like} alt=""/>
+                            <img src={comment} alt=""/>
+                            <img src={send} alt=""/>
+                        </div>
+
+                        <strong>900 curtidas</strong>
+
+                        <p>
+                            Um post muito massa.
+                            <span>#react #omnistack #top</span>
+                        </p>
+                    </footer>
+                </article>
+            </section>
         )
     }
 }
