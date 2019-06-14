b6c55712be02dd7cfe6ffce01e629765a6defe89
Robson P Oliveira <rpopuc@gmail.com>
2019-06-14 05:47:03 -0300

    Adiciona api para acesso ao backend
    - Altera frontend feed para uso da api

--- frontend/src/pages/Feed.js
+++ frontend/src/pages/Feed.js
@@ -7,65 +7,53 @@ import like from '../assets/like.svg';
 import comment from '../assets/comment.svg';
 import send from '../assets/send.svg';
 
+import api from '../services/api';
 
 class Feed extends Component {
+    state = {
+        feed: [],
+    };
+
+    async componentDidMount() {
+        const response = await api.get('posts');
+
+        this.setState({
+            feed: response.data,
+        })
+    }
+
     render() {
         return (
             <section id="post-list">
-                <article>
-                    <header>
-                        <div className="user-info">
-                            <span>Diego Fernandes</span>
-                            <span className="place">Rio do Sul</span>
-                        </div>
+                { this.state.feed.map(post => (
+                    <article>
+                        <header>
+                            <div className="user-info">
+                                <span>{post.author}</span>
+                                <span className="place">{post.place}</span>
+                            </div>
 
-                        <img src={more} alt="Mais" />
-                    </header>
+                            <img src={more} alt="Mais" />
+                        </header>
 
-                    <img src="http://localhost:3333/files/captura.jpg" alt=""/>
+                        <img src={`http://localhost:3333/files/${post.image}`} alt=""/>
 
-                    <footer>
-                        <div className="actions">
-                            <img src={like} alt=""/>
-                            <img src={comment} alt=""/>
-                            <img src={send} alt=""/>
-                        </div>
+                        <footer>
+                            <div className="actions">
+                                <img src={like} alt=""/>
+                                <img src={comment} alt=""/>
+                                <img src={send} alt=""/>
+                            </div>
 
-                        <strong>900 curtidas</strong>
+                            <strong>{post.likes} curtidas</strong>
 
-                        <p>
-                            Um post muito massa.
-                            <span>#react #omnistack #top</span>
-                        </p>
-                    </footer>
-                </article>
-                <article>
-                    <header>
-                        <div className="user-info">
-                            <span>Diego Fernandes</span>
-                            <span className="place">Rio do Sul</span>
-                        </div>
-
-                        <img src={more} alt="Mais" />
-                    </header>
-
-                    <img src="http://localhost:3333/files/captura.jpg" alt=""/>
-
-                    <footer>
-                        <div className="actions">
-                            <img src={like} alt=""/>
-                            <img src={comment} alt=""/>
-                            <img src={send} alt=""/>
-                        </div>
-
-                        <strong>900 curtidas</strong>
-
-                        <p>
-                            Um post muito massa.
-                            <span>#react #omnistack #top</span>
-                        </p>
-                    </footer>
-                </article>
+                            <p>
+                                {post.description}
+                                <span>{post.hashtag}</span>
+                            </p>
+                        </footer>
+                    </article>
+                )) }
             </section>
         )
     }
